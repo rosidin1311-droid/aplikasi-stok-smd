4e3da10137dcee2dc946b2f7b412e97f9a396da1
rosidin1311-droid <rosidin1311@gmail.com>
2026-05-28 06:39:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,17 +14,3 @@ def get_data():
 
 st.title("Aplikasi Stok Samindo")
 df = get_data()
-# Konfigurasi Halaman
-st.set_page_config(page_title="Data Stok SMD", layout="wide")
-
-# Koneksi
-conn = st.connection("gsheets", type=GSheetsConnection)
-
-# Ambil Data
-try:
-    df = conn.read(worksheet="produksi", ttl=0)
-    st.title("Aplikasi Stok Samindo")
-    st.write("Data berhasil dimuat!")
-    st.dataframe(df)
-except Exception as e:
-    st.error(f"Gagal memuat data: {e}")
