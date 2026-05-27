1f2178cc26e1d9107e9cfa1bad25036818f9cd40
rosidin1311-droid <rosidin1311@gmail.com>
2026-05-28 06:51:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Cara termudah: Menggunakan link publik
 # Ganti LINK_SHEET_ANDA dengan URL Google Sheet Anda
-url = "LINK_GOOGLE_SHEET_ANDA"
+url = "https://docs.google.com/spreadsheets/d/1fUlZzcHCHNgUYDwgvgcrn2e5B46HuWiREMIvQ_G1o28/edit?gid=0#gid=0"
 url = url.replace("/edit#gid=", "/export?format=csv&gid=")
 
 try:
