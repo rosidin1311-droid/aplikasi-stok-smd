598e3378bef615272be2fe3708446fe40592cd77
rosidin1311-droid <rosidin1311@gmail.com>
2026-05-28 06:56:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-# Cara termudah: Menggunakan link publik
-# Ganti LINK_SHEET_ANDA dengan URL Google Sheet Anda
-url = "https://docs.google.com/spreadsheets/d/1fUlZzcHCHNgUYDwgvgcrn2e5B46HuWiREMIvQ_G1o28/edit?gid=0#gid=0"
-url = url.replace("/edit#gid=", "/export?format=csv&gid=")
+st.set_page_config(page_title="Data Stok Samindo", layout="wide")
+st.title("🏭 APLIKASI DATA STOK SAMINDO")
+
+# Masukkan link CSV hasil 'Publish to web' tadi di sini
+CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSxAA8vyucxniOE_DKmYp6LmnxOw6EO676Xp0iEaOeKX7BKeVa2aVvOabU2Quf1Mccqsk8QUIh0UN-Q/pub?output=csv"
 
 try:
-    df = pd.read_csv(url)
-    st.write("Data Berhasil Dimuat!")
-    st.dataframe(df)
+    # Membaca data langsung sebagai tabel
+    df = pd.read_csv(CSV_URL)
+    st.success("Data berhasil dimuat!")
+    st.dataframe(df, width=1000)
 except Exception as e:
-    st.write("Belum terkoneksi, pastikan link sudah benar.")
+    st.error(f"Gagal memuat data: {e}")
